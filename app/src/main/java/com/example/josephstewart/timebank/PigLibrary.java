bf92b1fec457daa9ccda75a815884eb4f552f540
Zachary Leonard <zachl@ad.biola.edu>
2017-05-14 02:55:42 -0700

    Added Ashley's PigLibrary and UserData Java files

---
+++ app/src/main/java/com/example/josephstewart/timebank/PigLibrary.java
@@ -0,0 +1,53 @@
+package com.example.josephstewart.timebank;
+
+/**
+ * Created by Ashley on 5/13/2017.
+ */
+
+public class PigLibrary //storing class
+{
+    private final String SOURCE = "";
+    public final static String[] PIG_NAMES =
+            {
+                "Porky",
+                "Broken Porky",
+                "Sleepy Porky",
+                "Smarty Porky",
+                "Sporty Porky",
+                "Hammer Time",
+                "Porkerina",
+                "Porkzart",
+                "Food for Pork",
+                "Bibbityboppity Pork",
+
+
+            }; //saves all the pig names in an array
+    public final static int[] PIGS =
+            {
+                //JILL'S PICTURES NEED TO MATCH THE RED
+                R.drawable.porky, //resource location for default pig.
+                R.drawable.broken_pig, //resource location for broken pig.
+                R.drawable.sleepy_porky, //resource location for sleepy pig.
+                R.drawable.smart_porky, //resource location for smarty pig.
+                R.drawable.sporty_porky, //resource location for sporty pig.
+                R.drawable.handyman_porky, //resource location for default pig.
+                R.drawable.ballerina_porky, //resource location for default pig.
+                R.drawable.music_porky, //resource location for default pig.
+                R.drawable.shopping_porky, //resource location for default pig.
+                R.drawable.magic_porky, //resource location for default pig.
+
+            }; //saves the number of pigs? keeps track of pigs
+    public final static int DEFAULT = 0; //index location for default pig.
+    public final static int BROKEN = 1; //index location for default pig.
+    public final static int SLEEPY = 2; //index location for default pig.
+    public final static int SMARTY = 3; //index location for default pig.
+    public final static int SPORTY = 4; //index location for default pig.
+    public final static int HANDY = 5; //index location for default pig.
+    public final static int DANCY = 6; //index location for default pig.
+    public final static int MUSIC = 7; //index location for default pig.
+    public final static int SHOPPING = 8; //index location for default pig.
+    public final static int MAGIC = 9; //index location for default pig.
+
+
+
+}
---
+++ app/src/main/java/com/example/josephstewart/timebank/UserData.java
@@ -0,0 +1,89 @@
+package com.example.josephstewart.timebank;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ashley on 5/13/2017.
+ */
+
+public class UserData
+{
+    //private so we don't allow anyone to change the variable
+    private int coins, startTime, endTime;
+    private String currentPigTheme;
+    private ArrayList<String> unlockedPigThemes, whiteList;
+    private long timeUsed, timeAllowed;
+    public final static String DEFAULT = "default";
+
+    //constructor - default
+    public UserData()
+    {
+        coins = 0;
+        startTime = 0;
+        endTime = 2359;
+        timeUsed = 0;
+        timeAllowed = -1; //-1 means that there is no time / we are not tracking it rn
+                        //-1 means unlimited time
+        currentPigTheme = DEFAULT;
+        unlockedPigThemes = new ArrayList<String>();
+        unlockedPigThemes.add(DEFAULT); //adds default pig to the list of allowed pigs
+        whiteList = new ArrayList<String>();
+    }
+    public int getCoins()
+    {
+        return coins;
+    }
+    public int getStartTime()
+    {
+        return startTime;
+    }
+    public int getEndTime()
+    {
+        return endTime;
+    }
+    public boolean isLocked()
+    {
+        return ((timeUsed >= timeAllowed) || timeAllowed == -1);
+    }
+    public ArrayList<String> whiteList()
+    {
+        return whiteList;
+    }
+    public long getTimeUsed()
+    {
+        return timeUsed;
+    }
+
+    //modifiers
+    public void changeCoins(int i)
+    {
+        coins += i;
+    }
+    //validations
+    public boolean setStartTime(int i)
+    {
+        //check to make sure it is less than endtime and greater than 0 --> return true
+        if ((i < endTime) && (i > 0))
+        {
+            startTime = i;
+            return true;
+        }
+        return false;
+    }
+    public boolean setEndTime(int i)
+    {
+        //end time: check to make sure it is greater than start time and less than 2359
+        if ((i > startTime) && (i > 0))
+        {
+            endTime = i;
+            return true;
+        }
+        return false;
+    }
+    /*   public boolean changetimeUsed(long i)
+    {
+        //takes an long adds to time
+        return true;
+    }
+    */
+}
